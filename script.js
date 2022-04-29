1f5582859820d75c17bf4f36e9248c7e59ebe0c9
DmitryKisel85 <dim03@mail.ru>
2022-04-29 13:33:11 +0300

    'upd'

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 "use strict";
 
+//
+
+function watchForHover() {
+	// lastTouchTime is used for ignoring emulated mousemove events
+	let lastTouchTime = 0;
+
+	function enableHover() {
+		if (new Date() - lastTouchTime < 500) return;
+		document.body.classList.add("hasHover");
+	}
+
+	function disableHover() {
+		document.body.classList.remove("hasHover");
+	}
+
+	function updateLastTouchTime() {
+		lastTouchTime = new Date();
+	}
+
+	document.addEventListener("touchstart", updateLastTouchTime, true);
+	document.addEventListener("touchstart", disableHover, true);
+	document.addEventListener("mousemove", enableHover, true);
+
+	enableHover();
+}
+
+watchForHover();
+//
+
 const addTaskBtn = document.getElementById("add-task-btn");
 const clearAllBtn = document.getElementById("clear-all");
 const deskTaskInput = document.getElementById("input-main");
